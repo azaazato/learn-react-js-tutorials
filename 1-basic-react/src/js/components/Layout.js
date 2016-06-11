ef53bf37712e88237330b8cbbd633ecac19c43c4
azaazato <embryo314@gmail.com>
2016-06-11 15:16:29 +0900

    finish #4

--- 1-basic-react/src/js/components/Layout.js
+++ 1-basic-react/src/js/components/Layout.js
@@ -4,10 +4,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            title: "Welcome",
+        };
+    }
     render() {
+        setTimeout(() => {
+            this.setState({title: "Welcome Will!"});
+        }, 2000);
+
         return (
             <div>
-              <Header />
+              <Header title={this.state.title} />
+              <Header title={"Other Title"} />
               <Footer />
             </div>
         );
